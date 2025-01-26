4db099a0d99a48de9cb86e8097ba90a7cda6b6ef
Mauricio Gutierrez <142355026+AMGutierrezTrejos@users.noreply.github.com>
2025-01-25 21:00:50 -0500

    working on stories component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**', // Esto permite cualquier hostname con HTTPS
+          },
+        ],
+      },
+};
 
 export default nextConfig;
--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Stories = () => {
   return (
-    <div>Stories</div>
-  )
-}
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8">
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://i.redd.it/ebeh50lpw3i91.png"
+            alt=""
+            width={50}
+            height={50}
+            className="rounded-full"
+          />
+          <span className="font-medium">Mauro</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Stories
+export default Stories;
