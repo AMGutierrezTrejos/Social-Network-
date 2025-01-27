f3f19dce0d02cdd341a24faf5ae5cb886da344ec
Mauricio Gutierrez <142355026+AMGutierrezTrejos@users.noreply.github.com>
2025-01-26 23:04:45 -0500

    new post component

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const AddPost = () => {
   return (
-    <div>AddPost</div>
-  )
-}
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* PROFILE PIC */}
+      <Image
+        src="https://images3.alphacoders.com/135/1356879.png"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* POST */}
+      <div className="">
+        {/* INPUT */}
+        <div className="">
+          <textarea name="" id="" />
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor pointer self-end"
+          />
+        </div>
+        {/* POST OPTIONS */}
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
 
-export default AddPost
+export default AddPost;
--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -4,14 +4,70 @@ import React from "react";
 const Stories = () => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
-      <div className="flex gap-8">
+      <div className="flex gap-8 w-max">
+        {/* STORY */}
         <div className="flex flex-col items-center gap-2 cursor-pointer">
           <Image
-            src="https://i.redd.it/ebeh50lpw3i91.png"
+            src="https://static.wikia.nocookie.net/characterprofile/images/3/36/132Ditto.png/revision/latest?cb=20160809205529"
             alt=""
-            width={50}
-            height={50}
-            className="rounded-full"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Mauro</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://static.wikia.nocookie.net/characterprofile/images/3/36/132Ditto.png/revision/latest?cb=20160809205529"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Mauro</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://static.wikia.nocookie.net/characterprofile/images/3/36/132Ditto.png/revision/latest?cb=20160809205529"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Mauro</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://static.wikia.nocookie.net/characterprofile/images/3/36/132Ditto.png/revision/latest?cb=20160809205529"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Mauro</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://static.wikia.nocookie.net/characterprofile/images/3/36/132Ditto.png/revision/latest?cb=20160809205529"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Mauro</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://static.wikia.nocookie.net/characterprofile/images/3/36/132Ditto.png/revision/latest?cb=20160809205529"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
           />
           <span className="font-medium">Mauro</span>
         </div>
