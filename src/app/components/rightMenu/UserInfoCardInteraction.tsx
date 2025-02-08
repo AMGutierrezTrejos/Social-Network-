3a1312dd5196d146887811f6a384950f774937e8
Mauricio Gutierrez <142355026+AMGutierrezTrejos@users.noreply.github.com>
2025-02-08 15:52:47 -0500

    actions configure

--- src/app/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/app/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+import { switchFollow } from "@/lib/actions";
+import React, { useOptimistic, useState } from "react";
 
 const UserInfoCardInteraction = ({
   userId,
@@ -14,14 +15,58 @@ const UserInfoCardInteraction = ({
   isFollowing: boolean;
   isFollowingSent: boolean;
 }) => {
+  const [userState, setUserState] = useState({
+    following: isFollowing,
+    blocked: isUserBlocked,
+    followingRequestSent: isFollowingSent,
+  });
+
+  const follow = async () => {
+    switchOptimisticState("");
+    try {
+      await switchFollow(userId);
+      setUserState((prevState) => ({
+        ...prevState,
+        following: prevState.following && false,
+        followingRequestSent:
+          !prevState.following && !prevState.followingRequestSent
+            ? true
+            : false,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const block = async () => {
+
+  };
+
+  const [optimisticState, switchOptimisticState] = useOptimistic(
+    userState,
+    (state) => ({
+      ...state,
+      following: state.following && false,
+      followingRequestSent:
+        !state.following && !state.followingRequestSent ? true : false,
+    })
+  );
   return (
     <>
-      <button className="w-full bg-green-400 text-white text-sm rounded-md p-2">
-        {isFollowing ? "Following" : isFollowingSent ? "Friend Request Sent" : "Follow"}
-      </button>
-      <span className="text-red-400 self-end text-xs cursor-pointer">
-        {isUserBlocked ? "Unblock User" : "Block User"}
-      </span>
+      <form action={follow}>
+        <button className="w-full bg-green-400 text-white text-sm rounded-md p-2">
+          {optimisticState.following
+            ? "Following"
+            : optimisticState.followingRequestSent
+            ? "Friend Request Sent"
+            : "Follow"}
+        </button>
+      </form>
+      <form action="" className="self-end">
+        <span className="text-red-400  text-xs cursor-pointer">
+          {optimisticState.blocked ? "Unblock User" : "Block User"}
+        </span>
+      </form>
     </>
   );
 };
