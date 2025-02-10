24b99d09ef3120d07c4747164ec94e9f05b33543
Mauricio Gutierrez <142355026+AMGutierrezTrejos@users.noreply.github.com>
2025-02-09 21:38:48 -0500

    update profile

--- src/app/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/app/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { switchFollow } from "@/lib/actions";
+import { switchBlock, switchFollow } from "@/lib/actions";
 import React, { useOptimistic, useState } from "react";
 
 const UserInfoCardInteraction = ({
@@ -22,7 +22,7 @@ const UserInfoCardInteraction = ({
   });
 
   const follow = async () => {
-    switchOptimisticState("");
+    switchOptimisticState("follow");
     try {
       await switchFollow(userId);
       setUserState((prevState) => ({
@@ -39,17 +39,29 @@ const UserInfoCardInteraction = ({
   };
 
   const block = async () => {
-
+    switchOptimisticState("block");
+    try {
+      await switchBlock(userId);
+      setUserState((prevState) => ({
+        ...prevState,
+        blocked: !prevState.blocked,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const [optimisticState, switchOptimisticState] = useOptimistic(
     userState,
-    (state) => ({
-      ...state,
-      following: state.following && false,
-      followingRequestSent:
-        !state.following && !state.followingRequestSent ? true : false,
-    })
+    (state, value: "follow" | "block") =>
+      value === "follow"
+        ? {
+            ...state,
+            following: state.following && false,
+            followingRequestSent:
+              !state.following && !state.followingRequestSent ? true : false,
+          }
+        : { ...state, blocked: !state.blocked }
   );
   return (
     <>
@@ -62,10 +74,12 @@ const UserInfoCardInteraction = ({
             : "Follow"}
         </button>
       </form>
-      <form action="" className="self-end">
+      <form action={block} className="self-end">
+        <button>
         <span className="text-red-400  text-xs cursor-pointer">
           {optimisticState.blocked ? "Unblock User" : "Block User"}
         </span>
+        </button>
       </form>
     </>
   );
--- src/app/components/rightMenu/UserMediaCard.tsx
+++ src/app/components/rightMenu/UserMediaCard.tsx
@@ -2,8 +2,21 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { User } from "@prisma/client";
+import prisma from "@/lib/client";
 
-function UserMediaCard({ user }: { user: User }) {
+const UserMediaCard = async ({ user }: { user: User }) => {
+  const postsWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null,
+      },
+    },
+    take: 8,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="p-4 bg-white shadow-md rounded-lg text-sm flex flex-col gap-4">
       {/* TOP */}
@@ -15,17 +28,21 @@ function UserMediaCard({ user }: { user: User }) {
       </div>
       {/* BOT */}
       <div className="flex gap-4 justify-between flex-wrap">
-        <div className="relative w-1/5 h-24">
-          <Image
-            src="https://images.alphacoders.com/603/thumbbig-603479.webp"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
+        {postsWithMedia.length
+          ? postsWithMedia.map((post) => (
+              <div className="relative w-1/5 h-24" key={post.id}>
+                <Image
+                  src={post.img!}
+                  alt=""
+                  fill
+                  className="object-cover rounded-md"
+                />
+              </div>
+            ))
+          : "No Media Found!"}
       </div>
     </div>
   );
-}
+};
 
 export default UserMediaCard;
